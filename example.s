8ce2ec7444b9f2a2e26a9d1731fa7173bfb3722e
Mike Stepanov <timetocook420@gmail.com>
2024-12-05 16:01:05 +0300

    docs: added iwrite description in README

--- example.s
+++ example.s
@@ -13,7 +13,7 @@ producer_loop:
   div x15, x12, x10
   rem x16, x12, x10
   addi x31, x16, 48
-  sw x14, 0, x31
+  sw x14, x31, 0
   addi x14, x14, -1
   addi x12, x15, 0
   bne x12, x0, producer_loop
